ceafb1934a950495e368f14cac0ed1f8d46cf60a
unknown <sandeepshinde630@gmail.com>
2021-08-14 14:36:00 +0530

    feat:Ability to add Contact

---
+++ Day9Problem/AddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9Problem
+{
+    class AddressBook
+    {
+        public string FirstName, LastName, Address, City, State, Email;
+        public string PhoneNumber, Zip;
+
+        public AddressBook(string FirstName, string LastName, string Address, string City, string State, string PhoneNumber, string Zip, string Email)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.PhoneNumber = PhoneNumber;
+            this.Zip = Zip;
+            this.Email = Email;
+        }
+
+        public static void Index()
+        {
+
+            Console.WriteLine("Select Option");
+            Console.WriteLine("Enter 1 to Add Contact");
+            Console.WriteLine("Enter 2 to Exit");
+
+        }
+    }
+}
--- Day9Problem/Program.cs
+++ Day9Problem/Program.cs
@@ -9,6 +9,40 @@ namespace Day9Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Program");
+            LinkedList<AddressBook> list = new LinkedList<AddressBook>();
+            while (true)
+            {
+                AddressBook.Index();
+                int Check = Convert.ToInt32(Console.ReadLine());
+                switch (Check)
+                {
+                    case 1:
+                        Console.WriteLine("Enter FirstName");
+                        string FirstName = Console.ReadLine();
+                        Console.WriteLine("Enter LastName");
+                        string LastName = Console.ReadLine();
+                        Console.WriteLine("Enter Address");
+                        string Address = Console.ReadLine();
+                        Console.WriteLine("Enter City");
+                        string City = Console.ReadLine();
+                        Console.WriteLine("Enter State");
+                        string State = Console.ReadLine();
+                        Console.WriteLine("Enter PhoneNumber");
+                        string PhoneNumber = Console.ReadLine();
+                        Console.WriteLine("Enter Zip");
+                        string Zip = Console.ReadLine();
+                        Console.WriteLine("Enter Email");
+                        string Email = Console.ReadLine();
+
+                        list.AddLast(new AddressBook(FirstName, LastName, Address, City, State, PhoneNumber, Zip, Email));
+                        break;
+
+                    case 2:
+                        System.Environment.Exit(0);
+                        break;
+                }
+
+            }
         }
     }
 }
